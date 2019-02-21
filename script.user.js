2b34af778d3ac72106779cef5243dd95f4581b90
Matt Bague <mattbague@users.noreply.github.com>
2019-02-20 23:51:46 -0800

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -7,26 +7,45 @@
 // ==/UserScript==
 
 $("a[href*='u2=']").each((ndx, element) => {
-  const wrappedUrl = element.href;
-  const actualUrl = (new URL(wrappedUrl)).searchParams.get("u2");
-  $(element).attr("href", actualUrl);
+    const wrappedUrl = element.href;
+    const actualUrl = (new URL(wrappedUrl)).searchParams.get("u2");
+    $(element).attr("href", actualUrl);
 });
 
 $("a[href*='1=frontpage']").each((ndx, element) => {
-  const wrappedUrl = element.href
-  console.log(wrappedUrl)
-  GM_xmlhttpRequest({
-    url: wrappedUrl,
-    method: "GET",
-    anonymous: true,
-    onload: (res) => {
-      const actualUrl = new URL(res.finalUrl);
-      (qpExcludesByDomain[actualUrl.hostname] || []).map(qp => rawUrl.searchParams.delete(qp))
-      $(element).attr("href", actualUrl);
-    }
-  })
+    const wrappedUrl = element.href
+    console.log(wrappedUrl)
+    GM_xmlhttpRequest({
+        url: wrappedUrl,
+        method: "GET",
+        anonymous: true,
+        onload: (res) => {
+            const rawUrl = new URL(res.finalUrl);
+            const domain = rawUrl.hostname.startsWith("www.") ? rawUrl.hostname.replace("www.", "") : rawUrl.hostname
+            
+            const qpIncludes = siteQpIncludes[domain] || [];
+            const qpsToDelete = []
+            
+            let actualUrl;
+            if (qpIncludes.length > 0) {
+                rawUrl.searchParams.forEach((value, key) => {                    
+                    if (qpIncludes.indexOf(key) < 0) {                    
+                        qpsToDelete.push(key);
+                    }
+                });
+              
+              console.log(qpsToDelete);
+              qpsToDelete.map(k => rawUrl.searchParams.delete(k));
+
+              actualUrl = rawUrl.href
+            } else {
+                actualUrl = `${rawUrl.protocol}//${rawUrl.hostname}${rawUrl.pathname}`
+            }        
+            $(element).attr("href", actualUrl);
+          }
+    })
 });
 
-const qpExcludesByDomain = {
-  "newegg.com": ["sdtid", "SID", "AID", "PID", "nm_mc", "cm_mmc", "cm_sp"]
+const siteQpIncludes = {
+    "newegg.com": ["Item"]
 }
